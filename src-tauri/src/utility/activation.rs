3a91df4a86e406c666b42975f407808cceea4421
Mattia Italiano <mattia.italiano@csdental.com>
2023-10-11 18:40:32 +0100

    feat: adds reset_activation_client

--- src-tauri/src/utility/activation.rs
+++ src-tauri/src/utility/activation.rs
@@ -3,13 +3,13 @@ use std::process::Command;
 #[tauri::command]
 pub fn reset_activation_client() {
 
-    let ps_command = "Remove-Item -Path 'C:/ProgramData/TW/ActivationClient.db', 'C:/ProgramData/TW/ActivationClient.log' -Force";
+    let ps_command = r#"Remove-Item -Path 'C:/ProgramData/TW/ActivationClient.db', 'C:/ProgramData/TW/ActivationClient.log' -Force"#;
 
     Command::new("powershell")
         .args(&[
             "-Command",
-            &format!("Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \"{}\"' -Verb RunAs -WindowStyle Hidden", ps_command)
+            &format!(r#"Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command {}" -Verb RunAs -WindowStyle Hidden"#, ps_command)
         ])
         .status()
         .expect("failed to execute process");
-    }
+}
